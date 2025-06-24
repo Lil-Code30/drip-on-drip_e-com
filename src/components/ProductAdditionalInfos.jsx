9126750d674d86e46052a06da1cbf99d348f6aff
Ismael Loko <lokoismael30@gmail.com>
2025-06-24 05:57:03 -0400

    correcting details table error

--- src/components/ProductAdditionalInfos.jsx
+++ src/components/ProductAdditionalInfos.jsx
@@ -2,34 +2,42 @@ export default function ProductAdditionalInfos({ currentProduct }) {
   return (
     <>
       <table className="border my-2">
-        <tr>
-          <th className="text-left p-1 border bg-gray-300">
-            Warranty Information
-          </th>
-          <td className="p-1 border ">{currentProduct.warrantyInformation}</td>
-        </tr>
-        <tr>
-          <th className="text-left p-1 border bg-gray-300">
-            Shipping Information
-          </th>
-          <td className="p-1 border ">{currentProduct.shippingInformation}</td>
-        </tr>
-        <tr>
-          <th className="text-left p-1 border bg-gray-300">
-            Availability Status
-          </th>
-          <td className="p-1 border ">{currentProduct.availabilityStatus}</td>
-        </tr>
-        <tr>
-          <th className="text-left p-1 border bg-gray-300">Return Policy</th>
-          <td className="p-1 border ">{currentProduct.returnPolicy}</td>
-        </tr>
-        <tr>
-          <th className="text-left p-1 border bg-gray-300">
-            Minimum Order Quantity
-          </th>
-          <td className="p-1 border ">{currentProduct.minimumOrderQuantity}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <th className="text-left p-1 border bg-gray-300">
+              Warranty Information
+            </th>
+            <td className="p-1 border ">
+              {currentProduct.warrantyInformation}
+            </td>
+          </tr>
+          <tr>
+            <th className="text-left p-1 border bg-gray-300">
+              Shipping Information
+            </th>
+            <td className="p-1 border ">
+              {currentProduct.shippingInformation}
+            </td>
+          </tr>
+          <tr>
+            <th className="text-left p-1 border bg-gray-300">
+              Availability Status
+            </th>
+            <td className="p-1 border ">{currentProduct.availabilityStatus}</td>
+          </tr>
+          <tr>
+            <th className="text-left p-1 border bg-gray-300">Return Policy</th>
+            <td className="p-1 border ">{currentProduct.returnPolicy}</td>
+          </tr>
+          <tr>
+            <th className="text-left p-1 border bg-gray-300">
+              Minimum Order Quantity
+            </th>
+            <td className="p-1 border ">
+              {currentProduct.minimumOrderQuantity}
+            </td>
+          </tr>
+        </tbody>
       </table>
     </>
   );
--- src/components/ProductDescription.jsx
+++ src/components/ProductDescription.jsx
@@ -4,38 +4,44 @@ export default function ProductDescription({ currentProduct }) {
       <h3 className="text-lg font-semibold">Description:</h3>
       <p className="text-lg">{currentProduct.description}</p>
       <table>
-        <tr>
-          <th className="text-left p-1">Category:</th>
-          <td>{currentProduct.category}</td>
-        </tr>
-        <tr>
-          <th className="text-left p-1">Brand:</th>
-          <td>{currentProduct.brand}</td>
-        </tr>
-        <tr>
-          <th className="text-left p-1">Weight:</th>
-          <td>{currentProduct.weight}Kg</td>
-        </tr>
+        <tbody>
+          <tr>
+            <th className="text-left p-1">Category:</th>
+            <td>{currentProduct.category}</td>
+          </tr>
+          <tr>
+            <th className="text-left p-1">Brand:</th>
+            <td>{currentProduct.brand}</td>
+          </tr>
+          <tr>
+            <th className="text-left p-1">Weight:</th>
+            <td>{currentProduct.weight}Kg</td>
+          </tr>
+        </tbody>
       </table>
 
       <table className="border my-2">
-        <tr>
-          <th colSpan={2}>
-            <h3 className="text-lg font-semibold">Dimensions</h3>
-          </th>
-        </tr>
-        <tr>
-          <th className="border p-2 text-left">Width</th>
-          <td className="border p-1">{currentProduct.dimensions.width}</td>
-        </tr>
-        <tr>
-          <th className="border p-2 text-left">Height</th>
-          <td className="border p-1">{currentProduct.dimensions.height}</td>
-        </tr>
-        <tr>
-          <th className="border p-2 text-left">Depth</th>
-          <td className="border p-1">{currentProduct.dimensions.depth}</td>
-        </tr>
+        <thead>
+          <tr>
+            <th colSpan={2}>
+              <h3 className="text-lg font-semibold">Dimensions</h3>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th className="border p-2 text-left">Width</th>
+            <td className="border p-1">{currentProduct.dimensions.width}</td>
+          </tr>
+          <tr>
+            <th className="border p-2 text-left">Height</th>
+            <td className="border p-1">{currentProduct.dimensions.height}</td>
+          </tr>
+          <tr>
+            <th className="border p-2 text-left">Depth</th>
+            <td className="border p-1">{currentProduct.dimensions.depth}</td>
+          </tr>
+        </tbody>
       </table>
     </>
   );
