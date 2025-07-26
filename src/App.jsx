67ffc7c136b50a88bd050f451270b46fc3e5c634
Ismael Loko <lokoismael30@gmail.com>
2025-07-25 22:24:52 -0400

    moving products from localstorage to db

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import AppRouter from "./routes/AppRoutes";
-import { ProductProvider } from "./contexts/ProductsContext";
 import { WishListProvider } from "./contexts/WishListContext";
 import { CartProvider } from "./contexts/CartContext";
 import { UserProvider } from "./contexts/UserInfosContext";
@@ -7,18 +6,16 @@ import ToastNotify from "./components/common/ToastNotify";
 
 function App() {
   return (
-    <ProductProvider>
-      <UserProvider>
-        <CartProvider>
-          <WishListProvider>
-            <>
-              <ToastNotify />
-              <AppRouter />
-            </>
-          </WishListProvider>
-        </CartProvider>
-      </UserProvider>
-    </ProductProvider>
+    <UserProvider>
+      <CartProvider>
+        <WishListProvider>
+          <>
+            <ToastNotify />
+            <AppRouter />
+          </>
+        </WishListProvider>
+      </CartProvider>
+    </UserProvider>
   );
 }
 
